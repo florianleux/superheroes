d7fbc09519f8c33f10d203a3d5454cde5efed6d1
Florian Leux <florian.leux@gmail.com>
2020-09-11 09:55:23 +0200

    Création Liste Héros

---
+++ src/views/HeroesList.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-container fluid>
+        <v-row>
+            <v-col cols="2" v-for="hero in heroesList" :key="hero.id">
+                <v-card :title="hero.name" class="hero-card"
+                >
+                    <v-img class="picture"
+
+                           :src="hero.thumbnail.path+'/standard_fantastic.'+hero.thumbnail.extension"
+                    >
+                        <v-card-title class="name">
+                            {{hero.name}}
+                        </v-card-title>
+                    </v-img>
+                </v-card>
+
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HeroesList',
+  data: function() {
+      return {
+
+      }
+  },
+    props:['heroesList']
+
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+    .hero-card{
+        &:hover{
+            cursor: pointer;
+         }
+    }
+
+    .name{
+        color: white;
+        text-align: center;
+        display: block;
+        font-family: "Heroes Legend";
+        font-size: 0.8em;
+        bottom:0;
+        position: absolute;
+        left: 0;
+        right: 0;
+        word-break: normal;
+
+        -webkit-text-stroke: 1px black;
+        color: white;
+        text-shadow:
+                3px 3px 0 #000,
+                -1px -1px 0 #000,
+                1px -1px 0 #000,
+                -1px 1px 0 #000,
+                1px 1px 0 #000;
+    }
+</style>
