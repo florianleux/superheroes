a6f58414b945d5df4ca43b3054f1d456bf477e5c
Florian Leux <florian.leux@gmail.com>
2020-09-11 09:55:12 +0200

    Création Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-app-bar
+            shrink-on-scroll
+            dark
+            dense
+            height="25"
+    >
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Encyclopédie des super-héros Marvel</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar',
+  }
+</script>
