9f6adec0bf60da1087e06fc8df09a165d0b23c00
florianleux <florian.leux@gmail.com>
2020-09-28 15:18:04 +0200

    Reset et Update : Refacto

---
+++ src/views/List.vue
@@ -0,0 +1,112 @@
+<template>
+
+  <div>
+    <Pagination
+        @page-update="updatePage"
+        :page="page"
+        :list="list"
+    ></Pagination>
+    <v-container
+        fluid
+        class="hero-list"
+    >
+      <v-row>
+        <HeroCard
+            @select-hero="selectHero(hero)"
+            v-for="hero in heroesList.slice(heroesPerPage*(page-1),heroesPerPage*(page))"
+            :key="hero.id"
+            :hero="hero"
+        ></HeroCard>
+      </v-row>
+      <heroModal
+          :selected-hero="selectedHero"
+          v-if="heroModal"
+          @close-modal="heroModal = false"
+          :hero-modal="heroModal"
+          @update-hero="updateHero"
+          @reset-hero="updateHero"
+      />
+    </v-container>
+  </div>
+</template>
+<script>
+//TODO Ordre dans l'export !!
+import HeroCard from '@/components/HeroCard.vue'
+import HeroModal from '@/components/HeroModal.vue'
+import Pagination from '@/components/Pagination.vue'
+
+import {mapState, mapActions} from 'vuex';
+
+export default {
+  name: 'HeroesList',
+  components: {
+    HeroModal,
+    HeroCard,
+    Pagination,
+  },
+  data: function () {
+    return {
+      heroModal: false,
+      selectedHero: {},
+      page: 1,
+      heroesPerPage: process.env.VUE_APP_HEROES_PER_PAGE
+    }
+  },
+
+  methods: {
+    ...mapActions('heroes', [
+      'updateList',
+      'nextPage',
+      'updateHero',
+      'resetHero'
+    ]),
+    selectHero(hero) {
+      this.selectedHero = hero;
+      this.heroModal = true;
+      if (!hero.buffered) {
+        hero.bufferName = hero.name;
+        hero.bufferDescription = hero.description;
+        hero.buffered = true;
+      }
+    },
+    updatePage(newPage) {
+      this.page = newPage;
+    },
+  },
+  computed: {
+    ...mapState('heroes', [
+      'heroesList'
+    ])
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style
+    lang="scss"
+    scoped
+>
+.hero-list {
+  max-width: none;
+  padding: 0 10%;
+}
+
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+</style>
